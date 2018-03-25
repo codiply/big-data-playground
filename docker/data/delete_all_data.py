81c91772a690a8af86abe7d807957120f4d3df39
Panos <codiply@gmail.com>
2018-03-25 13:34:29 +0100

    Add probe service for probing kafka from command line

--- docker/data/delete_all_data.py
+++ docker/data/delete_all_data.py
@@ -1,10 +1,21 @@
 import os
+import shutil
 
 this_script = os.path.realpath(__file__)
-logs_dir = os.path.dirname(this_script)
+top_dir = os.path.dirname(this_script)
 
-for root, dirs, files in os.walk(logs_dir):
+for root, dirs, files in os.walk(top_dir):
     for f in files:
         filename = os.path.join(root, f)
-        if not (filename == this_script or f.startswith('.')):
+        if not (filename == this_script or f.endswith('.gitignore')):
             os.remove(filename)
+
+def directory_contains_files(dir):
+	for _, _, files in os.walk(dir):
+		if files:
+			return True
+	return False
+
+for root, _, _ in os.walk(top_dir):
+	if not directory_contains_files(root):
+		shutil.rmtree(root)
